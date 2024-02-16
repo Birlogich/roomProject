3ce8dfa14d5636e75b9b26fe025807ebed1394e5
Beerlogich <beerlogich@gmail.com>
2024-02-16 13:45:18 -0500

    Changed path to img

--- src/components/data/siteData.ts
+++ src/components/data/siteData.ts
@@ -6,7 +6,7 @@ interface PageInfo {
 
 const arrOfData: PageInfo[] = [
   {
-    mainPictureUrl: "../../src/assets/desktop-image-hero-1.jpg",
+    mainPictureUrl: "../../assets/desktop-image-hero-1.jpg",
     h1: "Discover innovative ways to decorate",
     mainDescription: `We provide unmatched quality, comfort, and style for
     property owners across the country. Our experts combine form and function 
@@ -14,7 +14,7 @@ const arrOfData: PageInfo[] = [
     collection and make your property a reflection of you and what you love.`,
   },
   {
-    mainPictureUrl: "../../src/assets/desktop-image-hero-2.jpg",
+    mainPictureUrl: "../../assets/desktop-image-hero-2.jpg",
     h1: "We are available all across the globe",
     mainDescription: `With stores all over the world, it's easy for you to find furniture 
     for your home or place of business. Locally, we’re in most major cities throughout the 
@@ -22,7 +22,7 @@ const arrOfData: PageInfo[] = [
     hesitate to contact us today.`,
   },
   {
-    mainPictureUrl: "../../src/assets/desktop-image-hero-3.jpg",
+    mainPictureUrl: "../../assets/desktop-image-hero-3.jpg",
     h1: "Manufactured with the best materials",
     mainDescription: `Our modern furniture store provide a high level of quality. Our company has invested in advanced 
     technology to ensure that every product is made as perfect and as consistent as possible. With three decades of 
