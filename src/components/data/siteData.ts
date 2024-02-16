613b31dbab835e83b990a186853248c7846e688d
Beerlogich <beerlogich@gmail.com>
2024-02-16 13:48:10 -0500

    Changed path to img

--- src/components/data/siteData.ts
+++ src/components/data/siteData.ts
@@ -1,3 +1,7 @@
+import imageHero1 from '../../assets/desktop-image-hero-1.jpg'
+import imageHero2 from '../../assets/desktop-image-hero-3.jpg'
+import imageHero3 from '../../assets/desktop-image-hero-2.jpg'
+
 interface PageInfo {
   mainPictureUrl : string,
   h1: string,
@@ -6,7 +10,7 @@ interface PageInfo {
 
 const arrOfData: PageInfo[] = [
   {
-    mainPictureUrl: "../../assets/desktop-image-hero-1.jpg",
+    mainPictureUrl: imageHero1,
     h1: "Discover innovative ways to decorate",
     mainDescription: `We provide unmatched quality, comfort, and style for
     property owners across the country. Our experts combine form and function 
@@ -14,7 +18,7 @@ const arrOfData: PageInfo[] = [
     collection and make your property a reflection of you and what you love.`,
   },
   {
-    mainPictureUrl: "../../assets/desktop-image-hero-2.jpg",
+    mainPictureUrl: imageHero2,
     h1: "We are available all across the globe",
     mainDescription: `With stores all over the world, it's easy for you to find furniture 
     for your home or place of business. Locally, we’re in most major cities throughout the 
@@ -22,7 +26,7 @@ const arrOfData: PageInfo[] = [
     hesitate to contact us today.`,
   },
   {
-    mainPictureUrl: "../../assets/desktop-image-hero-3.jpg",
+    mainPictureUrl: imageHero3,
     h1: "Manufactured with the best materials",
     mainDescription: `Our modern furniture store provide a high level of quality. Our company has invested in advanced 
     technology to ensure that every product is made as perfect and as consistent as possible. With three decades of 
